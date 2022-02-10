6be8dbaf7e47da46e31f1bfa514103ebb334d579
GILBERTO GARCIA DELGADO <MTRO.GILBERTO.GARCIA@GMAIL.COM>
2022-02-10 15:48:56 -0600

    git commit -m "avance"

--- Index.php
+++ Index.php
@@ -4,24 +4,24 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>StartUI - Premium Bootstrap 4 Admin Dashboard Template</title>
+	<title>SISTEMA:: ACCESO GILBERTO 123</title>
 
-	<link href="img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
-	<link href="img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
-	<link href="img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
-	<link href="img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
-	<link href="img/favicon.png" rel="icon" type="image/png">
-	<link href="img/favicon.ico" rel="shortcut icon">
+	<link href="Public/img/favicon.144x144.png" rel="apple-touch-icon" type="image/png" sizes="144x144">
+	<link href="Public/img/favicon.114x114.png" rel="apple-touch-icon" type="image/png" sizes="114x114">
+	<link href="Public/img/favicon.72x72.png" rel="apple-touch-icon" type="image/png" sizes="72x72">
+	<link href="Public/img/favicon.57x57.png" rel="apple-touch-icon" type="image/png">
+	<link href="Public/img/favicon.png" rel="icon" type="image/png">
+	<link href="Public/img/favicon.ico" rel="shortcut icon">
 
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!--[if lt IE 9]>
 	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
-<link rel="stylesheet" href="css/separate/pages/login.min.css">
-    <link rel="stylesheet" href="css/lib/font-awesome/font-awesome.min.css">
-    <link rel="stylesheet" href="css/lib/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="Public/css/separate/pages/login.min.css">
+    <link rel="stylesheet" href="Public/css/lib/font-awesome/font-awesome.min.css">
+    <link rel="stylesheet" href="Public/css/lib/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="Public/css/main.css">
 </head>
 <body>
 
@@ -30,26 +30,26 @@
             <div class="container-fluid">
                 <form class="sign-box">
                     <div class="sign-avatar">
-                        <img src="img/avatar-sign.png" alt="">
+                        <img src="Public/img/avatar-sign.png" alt="">
                     </div>
-                    <header class="sign-title">Sign In</header>
+                    <header class="sign-title">ACCESO</header>
                     <div class="form-group">
-                        <input type="text" class="form-control" placeholder="E-Mail or Phone"/>
+                        <input type="text" class="form-control" placeholder="Correo o Telefono"/>
                     </div>
                     <div class="form-group">
-                        <input type="password" class="form-control" placeholder="Password"/>
+                        <input type="password" class="form-control" placeholder="Contraseña"/>
                     </div>
                     <div class="form-group">
                         <div class="checkbox float-left">
                             <input type="checkbox" id="signed-in"/>
-                            <label for="signed-in">Keep me signed in</label>
+                            <label for="signed-in">Mantener registrado</label>
                         </div>
                         <div class="float-right reset">
-                            <a href="reset-password.html">Reset Password</a>
+                            <a href="reset-password.html">Restablecer Contraseña</a>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-rounded">Sign in</button>
-                    <p class="sign-note">New to our website? <a href="sign-up.html">Sign up</a></p>
+                    <button type="submit" class="btn btn-rounded">Entrar</button>
+                    <p class="sign-note">¿Eres Nuevo? <a href="sign-up.html">Entrar</a></p>
                     <!--<button type="button" class="close">
                         <span aria-hidden="true">&times;</span>
                     </button>-->
@@ -59,11 +59,11 @@
     </div><!--.page-center-->
 
 
-<script src="js/lib/jquery/jquery.min.js"></script>
-<script src="js/lib/tether/tether.min.js"></script>
-<script src="js/lib/bootstrap/bootstrap.min.js"></script>
-<script src="js/plugins.js"></script>
-    <script type="text/javascript" src="js/lib/match-height/jquery.matchHeight.min.js"></script>
+<script src="Public/js/lib/jquery/jquery.min.js"></script>
+<script src="Public/js/lib/tether/tether.min.js"></script>
+<script src="Public/js/lib/bootstrap/bootstrap.min.js"></script>
+<script src="Public/js/plugins.js"></script>
+    <script type="text/javascript" src="Public/js/lib/match-height/jquery.matchHeight.min.js"></script>
     <script>
         $(function() {
             $('.page-center').matchHeight({
@@ -80,6 +80,6 @@
             });
         });
     </script>
-<script src="js/app.js"></script>
+<script src="Public/js/app.js"></script>
 </body>
 </html>
