ff2cf954209831e717a358c80c9871d23a7d1db0
gamaliel <garciagomezgamaliel@gmail.com>
2022-04-06 01:25:26 -0500

    error al agrregar imagen en nosotros.php

--- nosotros.php
+++ nosotros.php
@@ -5,7 +5,7 @@
               <h3>bueno iniciemos mi nombre es Gamaliel Guadalupe Garcia Gomez, cabe redactar que esta es mi primer pagina web creada de este modo no tenia
                   la esperiencia de poder crear uno y esta es mi primera vez</h3>
             </center>
-           
+            
             <br><br><br><br><br><br><br><br><br><br><br><br><br>
          
             
@@ -20,4 +20,5 @@
 
             <center><h3> mi nombre es yahir ramirez luciano a diferencia de mi la elaboracion de esta pagina web como experiencia propia fue complicada en varios 
               aspectos debido a escoger adecuadamente los codigos y hacerlos funcional fue un reto que a un seguimos teniendo pero en teroria ya habia usado
-              antes otro lengua tal como html pero algo muy basico</center>
+              antes otro lenguaje tal como html pero algo muy basico</center>
+              <img src="../../Public/img/yahir..jpeg" alt="150" height="150"> <align="center">
